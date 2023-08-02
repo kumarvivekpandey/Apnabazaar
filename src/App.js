ec5c8c78443075dd008850690012ee7b287c3395
vivek kumar <vkpvivek1998@gmail.com>
2023-08-02 16:21:40 +0530

    updated
    updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import ProductList from './features/product-list/ProductList';
 
 function App() {
   return (
     <div className="App">
       <h1> hello this a ecommerce application which will be known as Apnabazaar</h1>
+      <ProductList />
     </div>
   );
 }
