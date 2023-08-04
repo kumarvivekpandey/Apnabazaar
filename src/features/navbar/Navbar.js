067ba9d40a8062e6706879a0e64b7857a6e4ebf3
vivek kumar <vkpvivek1998@gmail.com>
2023-08-04 14:56:09 +0530

    icon updated
    icon updated

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -6,8 +6,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 const user = {
     name: 'Tom Cook',
     email: 'tom@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    imageUrl:'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678130-profile-alt-4-512.png',
   }
   const navigation = [
     { name: 'Dashboard', href: '#', current: true },
