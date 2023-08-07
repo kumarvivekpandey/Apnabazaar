97ba6aea3570d1ca8057f3248cf843bc770967eb
vivek kumar <vkpvivek1998@gmail.com>
2023-08-07 15:50:25 +0530

    test
    test

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -271,13 +271,7 @@ export default function ProductList() {
                 {/* Filters */}
                 <form className="hidden lg:block">
                   <h3 className="sr-only">Categories</h3>
-                  <ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
-                    {subCategories.map((category) => (
-                      <li key={category.name}>
-                        <a href={category.href}>{category.name}</a>
-                      </li>
-                    ))}
-                  </ul>
+                
 
                   {filters.map((section) => (
                     <Disclosure as="div" key={section.id} className="border-b border-gray-200 py-6">
