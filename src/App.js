086f84fe5a9e40d6c6ef726242e59a6c7b8fc79c
vivek kumar <vkpvivek1998@gmail.com>
2023-08-08 15:28:59 +0530

    done
    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import Login from './pages/LoginPage';
 
 
 
@@ -9,7 +10,7 @@ function App() {
   return(
     <div className="App">
       <Home />
-    
+      <Login />  
     </div>
   
   );
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from '../features/Auth/login.js'
 
 export default function Login() {
   return (
     <div>
-        
+        <Login />
     </div>
   )
 }
