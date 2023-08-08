4d0a3652d11e8d0199d1899c5386795f5067c820
vivek kumar <vkpvivek1998@gmail.com>
2023-08-08 15:43:04 +0530

    done
    done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
-import Sighnin from './pages/Sighnin';
+
 
 
 
 function App() {
   return(
     <div className="App">
-       <Sighnin />
+      <h1>Hello</h1>
         <Home /> 
     
         
