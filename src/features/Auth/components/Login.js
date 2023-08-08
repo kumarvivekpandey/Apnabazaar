702b568b51049d3d6182c78b24ea3e101b3e68dc
vivek kumar <vkpvivek1998@gmail.com>
2023-08-08 15:16:33 +0530

    changes done
    changes done

---
+++ src/features/Auth/components/Login.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  increment,
+  incrementAsync,
+  
+} from './authSlice';
+import styles from './Counter.module.css';
+
+export function Counter() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const incrementValue = Number(incrementAmount) || 0;
+
+  return (
+    <div>
+      <div>
+       
+      </div>
+     
+    </div>
+  );
+}
