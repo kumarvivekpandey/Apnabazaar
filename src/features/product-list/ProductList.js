8e89a9d994600cbccb5904627a9690b49627543c
kumarvivekpandey <35356580+kumarvivekpandey@users.noreply.github.com>
2023-08-10 09:43:21 +0000

    change

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -270,7 +270,7 @@ export default function ProductList() {
 
             <section aria-labelledby="products-heading" className="pb-24 pt-6">
               <h2 id="products-heading" className="sr-only">
-                Products
+                Product
               </h2>
 
               <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
