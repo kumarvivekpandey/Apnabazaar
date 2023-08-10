b94e62ed8fc64951ec2c37aa81ed764330712714
vivek kumar <vkpvivek1998@gmail.com>
2023-08-10 17:19:40 +0530

    changes
    changes

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,10 +4,11 @@ import Navbar from '../features/navbar/Navbar'
 export default function Signup() {
   return (
     <div> 
-        <Navbar />
+       
 
 <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Navbar />
           <img
             className="mx-auto h-10 w-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
