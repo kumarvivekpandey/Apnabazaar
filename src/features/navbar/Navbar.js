673d7754741c7b402bec789b9362e90cfffb9284
vivek kumar <vkpvivek1998@gmail.com>
2023-08-11 12:54:45 +0530

    logo changes
    logo changes

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -40,7 +40,7 @@ function Navbar() {
                     <div className="flex-shrink-0">
                       <img
                         className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                        src="https://apnidukaan.com/media/apnidukaan-header/Apni-logo-white.png"
                         alt="Your Company"
                       />
                     </div>
